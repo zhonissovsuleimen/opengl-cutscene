1978142f255f7248def379053ecba36db674252e
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-04-29 00:38:34 -0400

    milestone1 updates and bugfix

--- src/scenarios/milestone1.cpp
+++ src/scenarios/milestone1.cpp
@@ -4,17 +4,41 @@
 #include <iostream>
 #include <filesystem>
 
+void setLightParams(std::vector<Light> *lights){
+  //hard-coded; might crush if the number of lights is different
+  (*lights)[0].intensity = 0.35f;
+  (*lights)[1].intensity = 0.35f;
+  (*lights)[2].intensity = 0.35f;
+  (*lights)[3].intensity = 0.35f;
+  (*lights)[4].intensity = 0.35f;
+  (*lights)[5].intensity = 0.35f;
+  (*lights)[6].intensity = 0.35f;
+  (*lights)[7].intensity = 0.35f;
+  (*lights)[8].intensity = 0.35f;
+
+  (*lights)[0].color = glm::vec3(1.0f, 1.0f, 1.0f);
+  (*lights)[1].color = glm::vec3(1.0f, 1.0f, 0.0f);
+  (*lights)[2].color = glm::vec3(1.0f, 0.0f, 1.0f);
+  (*lights)[3].color = glm::vec3(0.0f, 0.0f, 1.0f);
+  (*lights)[4].color = glm::vec3(0.0f, 0.0f, 1.0f);
+  (*lights)[5].color = glm::vec3(0.0f, 1.0f, 1.0f);
+  (*lights)[6].color = glm::vec3(0.5f, 0.0f, 0.5f);
+  (*lights)[7].color = glm::vec3(1.0f, 0.0f, 1.0f);
+  (*lights)[8].color = glm::vec3(1.0f, 1.0f, 1.0f);
+}
+
 void Milestone1::setup() {
   //requires cpp 17 or later
   std::string assets = (std::filesystem::current_path() / "assets" / "milestone1").string() + "/";
 
   Scene scene;
-  scene.setAmbient(glm::vec3(0.1f, 0.1f, 0.1f));
+  scene.setAmbient(glm::vec3(0.1f));
 
   Model model(assets + "model1.obj");
   scene.addModel(model);
 
   std::vector<Light> lights = Parser::parseLightCoords(assets + "light_coordinates.txt");
+  setLightParams(&lights);
   for(Light light : lights) { scene.addLight(light); }
 
   scene.camera = Camera(assets + "bezier.txt", glm::vec3(0.0f, 1.0f, 0.0f));
@@ -40,5 +64,5 @@ void Milestone1::play(){
     scene.camera.animate(t);
   }
 
-  std::cin.get();
+  while(renderer.renderScene(&scene)){}
 }
